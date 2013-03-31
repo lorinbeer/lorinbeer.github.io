911391ded6c1f310d8fbba510d63bd5c536c3d47
lorinbeer <lorin@adobe.com>
2013-03-30 17:07:16 -0700

    added sequence 2

--- show/enter-the-phonegap/phonegap.js
+++ show/enter-the-phonegap/phonegap.js
@@ -1,29 +1,40 @@
+var frame1delta = '1s';
 var js = new Rect(0, 0, 100, 100).fill("green").addTo(stage);
 
 var css = new Rect(0, 0, 100, 100).fill("red").addTo(stage);
 
 var html = new Rect(0, 0, 100, 100).fill("blue").addTo(stage);
 
-var jskeyanim = new KeyframeAnimation('5s', {
+var container = new Rect(90,90,160,160).stroke('white','5');
+
+function htmlsequence2() {
+  container.addTo(stage).animate('2s',{strokeColor:'black'});
+  js.animate(jskeyanim2);
+  css.animate(csskeyanim2);
+  html.animate(htmlkeyanim2);  
+}
+
+var jskeyanim = new KeyframeAnimation(frame1delta, {
    '0%'  : { x: 0, y: 0, easing : 'quartOut' },
    '50%' : { x: 50, y:0, easing : 'quartOut'  },
    '75%' : { x: 50, y:0 } ,
    '100%': { x: 100, y: 100, easing : 'quartOut'}
 });
 
-var csskeyanim = new KeyframeAnimation('5s', {
+var csskeyanim = new KeyframeAnimation(frame1delta, {
    '0%'  : { x: 0, y: 0, easing : 'quartOut' },
    '50%' : { x: 200, y:0, easing : 'quartOut'  },
    '75%' : { x: 200, y:0 } ,
    '100%': { x: 120, y: 120, easing : 'quartOut'}
 });
 
-var htmlkeyanim = new KeyframeAnimation('5s', {
+var htmlkeyanim = new KeyframeAnimation(frame1delta, {
    '0%'  : { x: 0, y: 0, easing : 'quartOut' },
    '50%' : { x: 350, y:0, easing : 'quartOut'  },
    '75%' : { x: 350, y:0 } ,
-   '100%': { x: 140, y: 140, easing : 'quartOut'}
-});
+   '100%': { x: 140, y: 140, easing : 'quartOut'}},
+   {onEnd: function() {htmlsequence2();}}
+); 
 
 js.animate(jskeyanim);
 css.animate(csskeyanim);
