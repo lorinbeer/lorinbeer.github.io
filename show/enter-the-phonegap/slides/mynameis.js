8e561a4c302ba01b2384c6643ec343f19c593712
lorinbeer <lorin@adobe.com>
2013-04-01 00:18:23 -0700

    content for speaker intro page

---
+++ show/enter-the-phonegap/slides/mynameis.js
@@ -0,0 +1,18 @@
+
+new Text('Lorin Beer')
+.addTo(stage);
+
+new Bitmap('../assets/lorinbeer.jpg')
+.addTo(stage);
+
+new Text('Software Developer at Adobe Systems')
+.addTo(stage);
+
+new Text('Apache Commiter on the PhoneGap/Cordova Project')
+.addTo(stage);
+
+new Text('Cordova Contributor: iOS, Android, Blackberry, MobileSpec, Docs')
+.addTo(stage);
+
+new Text('Creator of the Pender Project: Hardware Accelerated Canvas on Mobile')
+.addTo(stage); 
