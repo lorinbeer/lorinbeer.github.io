712ac918395180f583f8146f1bebf6186c2f3896
lorinbeer <lorin@adobe.com>
2013-04-01 08:00:02 -0700

    adjusted for resolution

--- show/enter-the-phonegap/slides/whomakesthishappen.js
+++ show/enter-the-phonegap/slides/whomakesthishappen.js
@@ -1,35 +1,36 @@
 
 
-new Rect(0,0,10240,768)
+new Rect(0,0,1024,768)
 .attr({fillColor: 'white'})
 .addTo(stage);
 
 
 new Bitmap('../assets/contributers/adobe.jpg')
-.attr({x:-100,y:50})
-.animate('2s', {x: 0, y: 50})
+.attr({x:-350,y:50})
+.animate('1s', {x: -100, y: 50}, {easing:'expoIn'})
 .addTo(stage);
 
 
 new Bitmap('../assets/contributers/google.png')
 .attr({x:1000,y:50})
-.animate('2s', {x: 375, y: 145})
+.animate('1s', {x: 240, y: 145}, {easing:'expoIn'})
 .addTo(stage);
 
 
 new Bitmap('../assets/contributers/ibm.png')
-.animate('2s', {x: 140, y: 450})
+.attr({x:140,y:-200})
+.animate('1s', {x: 140, y: 450}, {easing:'expoIn'})
 .addTo(stage);
 
 
 new Bitmap('../assets/contributers/intel.png')
 .attr({x:1000,y:450})
-.animate('2s', {x: 800, y: 50})
+.animate('1s', {x: 620, y: 50}, {easing:'expoIn'})
 .addTo(stage);
 
 
 new Bitmap('../assets/contributers/rim.jpg')
 .attr({x:1000,y:450})
-.animate('2s', {x: 700, y:450})
+.animate('1s', {x: 600, y:450}, {easing:'expoIn'})
 .addTo(stage);
 
