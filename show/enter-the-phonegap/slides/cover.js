8c527c6416d27324cc0046b7479ca3a352ceec90
lorinbeer <lorin@adobe.com>
2013-04-01 00:21:14 -0700

    cover page content

---
+++ show/enter-the-phonegap/slides/cover.js
@@ -0,0 +1,8 @@
+
+
+new Bitmap('../assets/logo-phonegap.png')
+.addTo(stage);
+
+new Text('Lorin Beer').addTo(stage);
+new Text('@doggerelverse').addTo(stage);
+new Text('www.ensufire.com').addTo(stage);
