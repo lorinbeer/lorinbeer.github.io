da5e3ba7176c1e4eae773c363f0c93836cb19145
lorinbeer <lorin@adobe.com>
2013-04-01 07:34:09 -0700

    cover, animated logo and text

--- show/enter-the-phonegap/slides/cover.js
+++ show/enter-the-phonegap/slides/cover.js
@@ -1,8 +1,27 @@
+function addText(text,sx,sy,dx,dy,t,fs) {
+    new Text(text)
+        .attr({x: sx,
+               y: sy,
+               textFillColor : 'black',
+               fontFamily: 'Arial, sans-serif',
+               fontSize:fs})
+        .animate(t,{x: dx,
+                    y: dy }, {
+                    easing: 'expoOut'
+                    })
+        .addTo(stage);
+}
 
-
-new Bitmap('../assets/logo-phonegap.png')
+new Rect(0,0,1100,800)
+.attr({fillColor:'white'})
 .addTo(stage);
 
-new Text('Lorin Beer').addTo(stage);
-new Text('@doggerelverse').addTo(stage);
-new Text('www.ensufire.com').addTo(stage);
+new Bitmap('../assets/logo-phonegap.png')
+.attr({x:-300, y: 75})
+.animate('1.5s',{x:50, y:75})
+.addTo(stage);
+
+addText('Enter The', 1400, 50, 50, 25, '0.5s', '60');
+addText('Lorin Beer', 500, 800, 500, 500,'2s', '35');
+addText('@doggerelverse', 500, 800, 500, 550,'2s', '35');
+addText('www.ensufire.com', 500, 800, 500, 600,'2s','35');
