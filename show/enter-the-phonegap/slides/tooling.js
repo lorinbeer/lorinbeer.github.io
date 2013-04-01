10534ed745bb244b73a8c349cf462178d56db6ed
lorinbeer <lorin@adobe.com>
2013-03-31 23:31:28 -0700

    first pass cordova tooling movie

---
+++ show/enter-the-phonegap/slides/tooling.js
@@ -0,0 +1,23 @@
+
+function addText(text,sx,sy,dx,dy) {
+
+    new Text(text)
+        .attr({x: sx,
+               y: sy,
+               textFillColor : 'white',
+               fontFamily: 'Arial, sans-serif',
+               fontSize:'50'})
+        .animate('2s',{x: dx,
+                       y: dy})
+        .addTo(stage);
+}
+
+addText('Cordova CLI', -300, 100, 100, 100);
+
+addText('Cordova Docs',-300,-300, 100, 200);
+
+addText('Cordova Mobile Spec', 1000, 1000, 200, 300);
+
+addText('Weinre (Web Inspector Remote',1000,1000,100,400);
+
+addText('Cordova COHO',1000,-100, 200, 500);
