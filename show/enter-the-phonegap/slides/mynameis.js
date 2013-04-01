19d9c7f2cf1e551a7f5a45554a8a0f2669a093fe
lorinbeer <lorin@adobe.com>
2013-04-01 07:25:51 -0700

    final placement, anim timing parameter to text add and easing

--- show/enter-the-phonegap/slides/mynameis.js
+++ show/enter-the-phonegap/slides/mynameis.js
@@ -1,12 +1,12 @@
 
 
-function addText(text,sx,sy,dx,dy,t) {
+function addText(text,sx,sy,dx,dy,t,fs) {
     new Text(text)
         .attr({x: sx,
                y: sy,
                textFillColor : 'white',
                fontFamily: 'Arial, sans-serif',
-               fontSize:'50'})
+               fontSize:fs})
         .animate(t,{x: dx,
                     y: dy }, {
                     easing: 'expoOut'
@@ -21,13 +21,18 @@ new Rect(1200,800,-10,-10)
 new Bitmap('../assets/lorinbeer.jpg')
 .addTo(stage);
 
-addText('Lorin Beer', 1400, 25, 1000, 25, '0.5s');
-/*
-addText('Software Developer at Adobe Systems',-300,-300, 100, 200);
+addText('Lorin Beer', 1400,20, 750, 20, '0.5s', '50');
 
-addText('Apache Commiter on the PhoneGap/Cordova Project', 1000, 1000, 200, 300);
+addText('Software Developer at Adobe',500,1000, 500, 150,'1s','30');
 
-addText('Cordova Contributor: iOS, Android, Blackberry, MobileSpec, Docs',1000,1000,100,400);
+addText('Apache Commiter', 500, 1000, 500, 225,'1s','30');
 
-addText('Pender Project: Hardware Accelerated Canvas on Mobile',1000,-100, 200, 500);
-*/
+addText('PhoneGap/Cordova Contributor:',500,1000,500,300,'1s','30');
+addText('iOS, Android, Blackberry',550,1000,550,340,'1s','30');
+addText('MobileSpec, Docs',550,1000,550,380,'1s','30');
+
+addText('Pender Project:' , 500, 1000, 500, 450,'1s','30');
+addText('Mobile Accelerated Canvas', 550, 1000, 550, 480,'1s','30');
+
+addText('@doggerelverse', 500, -500, 500, 550,'2s','28');
+addText('www.ensufire.com',500, -500, 500, 600, '2s', '28');
